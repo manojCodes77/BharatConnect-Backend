1a55610b07494e5f03f2bcc68463ba25e9c60970
Manoj Singh Rawat <natkhatkanharawatji@gmail.com>
2025-11-08 11:54:07 +0530

    adding user routes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express, {Express, Request, Response} from 'express';
+import userRoutes from './routes/user-route';
+import postRoutes from './routes/post-route';
 import connectDB from './config/db';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -8,9 +10,12 @@ const app: Express = express();
 const port = process.env.PORT || 8080;
 
 connectDB(process.env.MONGO_URI || '');
+app.use(express.json());
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, world!');
 });
+app.use('/api/v1/users',userRoutes);
+app.use('/api/v1/posts',postRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
--- src/routes/user-route.ts
+++ src/routes/user-route.ts
@@ -1,15 +1,73 @@
 import { Router } from "express";
+import User from "../models/user-model";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 const router = Router();
 
-router.post('/sign-up', (req, res) => {
-    // Handle user sign-up
-    res.send('User signed up');
+router.post('/sign-up', async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        if(!name || !email || !password) {
+            return res.status(400).json({ message: 'All fields are required' ,success: false});
+        }
+        // first check if user already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists' ,success: false});
+        }
+        const newUser = new User({ name, email, password });
+        await newUser.save();
+        res.status(201).json({ message: 'User signed up' ,success: true});
+    } catch (error) {
+        console.error("Error signing up user:", error);
+        res.status(500).json({ message: 'Internal Server Error' ,success: false});
+    }
 });
 
-router.post('/sign-in', (req, res) => {
-    // Handle user sign-in
-    res.send('User signed in');
+router.post('/sign-in', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if(!email || !password) {
+            return res.status(400).json({ message: 'Email and password are required' ,success: false});
+        }
+        const user = await User.findOne({ email });
+        if (!user || user.password !== password) {
+            return res.status(401).json({ message: 'Invalid credentials' ,success: false});
+        }
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET||"", { expiresIn: '1h' });
+        res.json({ message: 'User signed in', token });
+    } catch (error) {
+        console.error("Error signing in user:", error);
+        res.status(500).json({ message: 'Internal Server Error' ,success: false});
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id).select('-password');
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' ,success: false});
+        }
+        res.json(user);
+    } catch (error) {
+        console.error("Error fetching user:", error);
+        res.status(500).json({ message: 'Internal Server Error' ,success: false});
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find().select('-password');
+        if(!users || users.length === 0) {
+            return res.status(404).json({ message: 'No users found' ,success: false});
+        }
+        res.json(users);
+    } catch (error:any) {
+        console.error("Error fetching users:", error.message);
+        res.status(500).json({ message: 'Internal Server Error' ,success: false});
+    }
 });
 
 export default router;
